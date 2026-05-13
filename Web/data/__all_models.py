3ce5e47dbb9e0658d989d6612df85f25a04f0a59
Dozhder <zyablicev.dk1@gmail.com>
2026-05-14 03:04:13 +0500

    Delete Web directory

--- Web/data/__all_models.py
+++
@@ -1,5 +0,0 @@
-from . import users
-from . import products
-from . import orders
-from . import carts
-from . import eateries
--- Web/forms/add_address.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, EmailField, BooleanField, StringField, DateField, SelectField
-from wtforms.validators import DataRequired
-
-
-class AddAddressForm(FlaskForm):
-    address = StringField('Адрес', validators=[DataRequired()])
-    submit = SubmitField('Submit')
