6b326a0d51724747d69e5c59c81279d0815ea1aa
Dozhder <zyablicev.dk1@gmail.com>
2026-05-14 02:28:49 +0500

    Delete WEB_Project directory

--- WEB_Project/Web/forms/currier_selection.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired
-
-
-class CurrierSelectionForm(FlaskForm):
-    currier = SelectField('Выберете курьера', choices=[])
-    submit = SubmitField('Submit')
